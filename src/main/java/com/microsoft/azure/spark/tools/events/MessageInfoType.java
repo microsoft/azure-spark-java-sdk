576a295b36fa2b73b7c07255b58a347c0361d399
Wei Zhang <zhwe@microsoft.com>
2019-08-06 11:34:09 +0800

    Fix Message Info Type with fixed integer values

--- src/main/java/com/microsoft/azure/spark/tools/events/MessageInfoType.java
+++ src/main/java/com/microsoft/azure/spark/tools/events/MessageInfoType.java
@@ -4,10 +4,24 @@
 package com.microsoft.azure.spark.tools.events;
 
 public enum MessageInfoType {
-    Error,
-    Info,
-    Log,
-    Warning,
-    Hyperlink,
-    HyperlinkWithText
+    Trace(10),
+    Debug(20),
+    Info(30),
+    Warning(40),
+    Error(50),
+    Fatal(60),
+
+    Log(100),
+
+    Hyperlink(1000);
+
+    private final int value;
+
+    MessageInfoType(final int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
